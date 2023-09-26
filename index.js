638e5bd7aed82b79342a2ccc89254bb1ffd515af
Your Name <mikachupichu@gmail.com>
2023-09-26 10:17:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array){
+    const winningrecord= array.find(function (array){
+        return array.result ==="W";   })
+    return winningrecord?winningrecord.year: undefined;
+}
+
